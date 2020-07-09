4d8aee40748153f882e6147b267a645d42616cfc
tenfy <tenfy@tenfy.cn>
2020-07-09 10:03:03 +0800

    Init.
    Get containers use docker-client and print the networks.

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/tenfyzhong/ifc
+
+go 1.14
+
+require (
+	github.com/fsouza/go-dockerclient v1.6.5
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/urfave/cli v1.22.4
+)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/urfave/cli"
+)
+
+var (
+	host       string
+	tls        bool
+	tlscacert  string
+	tlscert    string
+	tlskey     string
+	tlsverify  bool
+	help       bool
+	maxNameLen int
+)
+
+func main() {
+	home, err := homedir.Dir()
+	capath := ""
+	certpath := ""
+	keypath := ""
+	if err == nil {
+		capath = filepath.Join(home, ".docker", "ca.pem")
+		certpath = filepath.Join(home, ".docker", "cert.pem")
+		keypath = filepath.Join(home, ".docker", "key.pem")
+	}
+
+	app := cli.NewApp()
+	app.HideHelp = true
+	app.Name = "ifc"
+	app.Usage = "interface configuration of docker container"
+	app.Version = "0.1.0"
+	app.ArgsUsage = "ifc"
+	app.Copyright = "Copyright (c) 2020 tenfy"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{Name: "tenfy", Email: "tenfy@tenfy.cn"},
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "host, H",
+			Value:       "unix:///var/run/docker.sock",
+			Usage:       "Daemon `socket` to connect to",
+			Destination: &host,
+		},
+		cli.BoolFlag{
+			Name:        "tls",
+			Usage:       "Use TLS; implied by --tlsverify",
+			Destination: &tls,
+		},
+		cli.StringFlag{
+			Name:        "tlscacert",
+			Usage:       "Trust certs signed only by this CA",
+			Value:       capath,
+			Destination: &tlscacert,
+		},
+		cli.StringFlag{
+			Name:        "tlscert",
+			Usage:       "Path to TLS certificate file",
+			Value:       certpath,
+			Destination: &tlscert,
+		},
+		cli.StringFlag{
+			Name:        "tlskey",
+			Usage:       "Path to TLS key file",
+			Value:       keypath,
+			Destination: &tlskey,
+		},
+		cli.BoolFlag{
+			Name:        "tlsverify",
+			Usage:       "Use TLS and verity the remote",
+			Destination: &tlsverify,
+		},
+		cli.IntFlag{
+			Name:        "max_name_len",
+			Usage:       "Max name len",
+			Value:       20,
+			Destination: &maxNameLen,
+		},
+		cli.BoolFlag{
+			Name:        "help, h",
+			Usage:       "Show help",
+			Destination: &help,
+		},
+	}
+
+	if maxNameLen == 0 {
+		maxNameLen = 20
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if help {
+			cli.ShowAppHelpAndExit(c, 0)
+		}
+		client, err := newClient()
+		if err != nil {
+			return err
+		}
+
+		ifc(client)
+		return nil
+	}
+
+	app.Run(os.Args)
+}
